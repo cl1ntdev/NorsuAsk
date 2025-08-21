d8503dc094218c63655045c80104f3b7ff8f8253
cl1ntdev <clintjayestrellanes17@gmail.com>
2025-08-21 21:11:17 +0800

    color for debugging

--- utils/Page/Default.tsx
+++ utils/Page/Default.tsx
@@ -1,8 +1,8 @@
 const Header = () =>{
   return(
     <div className="justify-center">
-      <a className="my-4">Qustion</a>
-      <a className="my-4">About</a>
+      <a className="mx-4 mt-5 bg-amber-50 text-blue-600">Qustion</a>
+      <a className="mx-4 mt-5  bg-amber-50 text-blue-600">About</a>
     </div>
   )
 }
