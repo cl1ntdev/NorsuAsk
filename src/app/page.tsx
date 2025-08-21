9a2355af6b4bd65ae262a1b1bd42616663c8c64b
cl1ntdev <clintjayestrellanes17@gmail.com>
2025-08-21 21:44:09 +0800

    passing val to func

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,24 @@
 'use client'
 
-import React from "react";
+import React, { useEffect } from "react";
 import { Header } from "./utils/_Components/Header";
 import { useState } from "react";
 
 
 export default function Home() {
-  const [page,setPage] = useState<string>("")
+  const [page, setPage] = useState<string>("Question")
+  const handleChangePage = (value:string) =>{
+    setPage(value)
+  }
+  
+  useEffect(()=>{
+    console.log(page)
+  },[page])
   
   return (
     <div className="flex justify-center align-middle bg-red-500 pt-2">
-      <Header />
+      <Header changePageStatus={handleChangePage} />
+      <a>{page}</a>
     </div>
   );
 }
--- src/app/utils/_Components/Header.tsx
+++ src/app/utils/_Components/Header.tsx
@@ -1,10 +1,15 @@
 import React from "react"
 
-const Header = () =>{
+type pageStatus = {
+  changePageStatus:(value:string) => void
+}
+// pass the value using prop first(child) then to the parent file
+
+const Header = ({changePageStatus}:pageStatus) =>{
   return(
     <div className="justify-center">
-      <a className="mx-4 mt-5 bg-amber-50 text-blue-600">Qustion</a>
-      <a className="mx-4 mt-5  bg-amber-50 text-blue-600">About</a>
+      <button onClick={()=>changePageStatus("Question")} className="mx-4 mt-5 bg-amber-50 text-blue-600">Qustion</button>
+      <button onClick={()=>changePageStatus("About")} className="mx-4 mt-5  bg-amber-50 text-blue-600">About</button>
     </div>
   )
 }
