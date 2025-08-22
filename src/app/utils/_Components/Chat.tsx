7aee726cb64c95ab357f46a22768413e78c75295
cl1ntdev <clintjayestrellanes17@gmail.com>
2025-08-22 18:15:44 +0800

    chat structure

--- src/app/utils/_Components/Chat.tsx
+++ src/app/utils/_Components/Chat.tsx
@@ -1,11 +1,18 @@
 import React, { useState} from "react";
 
 export default function Chat(){
-  const 
+  const [userMessage,setUserMessage] = useState<string>("")
   
+  const handleHistory = () =>{
+    console.log("working")
+  }
   return(
     <div>
       
+      <div>
+        <input type="text" placeholder="Ask Norsu Ai" value={userMessage} />
+        <button onClick={handleHistory}>Send</button>
+      </div>
     </div>
   )
 }
