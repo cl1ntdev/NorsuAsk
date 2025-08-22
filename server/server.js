909cd8bd068c55e87eaa441464c9498d1735f0d2
cl1ntdev <clintjayestrellanes17@gmail.com>
2025-08-22 21:00:45 +0800

    added cors

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import Groq from 'groq-sdk'
+import cors from 'cors'
 
 dotenv.config()
 console.log("DEBUG: env key =", process.env.GROQ_API_KEY);
 const app = express()
 app.use(express.json())
+app.use(cors({origin:"*"}))
 
 const groq = new Groq({apiKey:process.env.GROQ_API_KEY})
 
