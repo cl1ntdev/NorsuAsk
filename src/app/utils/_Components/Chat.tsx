deb8de96516e7294abc52f6f90954d43d6c03cc4
cl1ntdev <clintjayestrellanes17@gmail.com>
2025-08-22 19:07:43 +0800

    fix chat structure

--- src/app/utils/_Components/Chat.tsx
+++ src/app/utils/_Components/Chat.tsx
@@ -1,7 +1,23 @@
 import React, { useState} from "react";
 
+type ChatType = {
+  sender: "ai" | "user",
+  message: string
+}
+
 export default function Chat(){
   const [userMessage,setUserMessage] = useState<string>("")
+  
+  const [chats,setChat] = useState<ChatType[]>([
+    {
+      sender:"ai",
+      message:"Hello"
+    },
+    {
+      sender:"user",
+      message:"World"
+    }
+  ])
   
   const handleHistory = () =>{
     console.log("working")
@@ -10,7 +26,11 @@ export default function Chat(){
     <div className="flex flex-col h-screen w-screen bg-amber-700">
       {/* Chat Box */}
       <div className="flex-1 overflow-y-auto"> 
-        <p className="text-zinc-950 bg-amber-300">Testing</p>
+        {chats.map((chat,key)=>(
+          <div className={`flex ${chat.sender == "ai" ? "justify-start" : "justify-end"} bg-red-500`} key={key}>
+            <p>{chat.sender}: {chat.message}</p>
+          </div>
+        ))}
       </div>
       
       {/* ========= */}
@@ -18,7 +38,7 @@ export default function Chat(){
       {/* ========= */}
       
       <div className="flex items-center justify-center mb-5">
-        <input className="mx-5" type="text" placeholder="Ask Norsu Ai" value={userMessage} />
+        <input className="mx-5" type="text" placeholder="Ask Norsu Ai" value={userMessage} onChange={()=>setUserMessage} />
         <button className="mx-5" onClick={handleHistory}>Send</button>
       </div>
     </div>
