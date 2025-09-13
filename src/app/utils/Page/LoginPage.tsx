ea320ecd0e51a3130d58f7a5c85f4bde61e0f95b
cl1ntdev <clintjayestrellanes17@gmail.com>
2025-09-14 00:31:51 +0800

    implement auth

--- src/app/utils/Page/LoginPage.tsx
+++ src/app/utils/Page/LoginPage.tsx
@@ -1,11 +1,23 @@
 import React, { useState} from "react";
+import { useAuth } from "../Hooks/AuthHook";
 
 export default function LoginPage(){
+  const {login} = useAuth()
+  const [user,setUser] = useState<string>("")
+  const [pass,setPass] = useState<string>("")
+  
+  const handleLogin = () =>{
+    const newUser = {
+      "username":user,
+      "password":pass
+    } 
+    login(newUser);
+  }
   return(
     <div>
-      <input className="text-black" placeholder="Enter Username" />
-      <input className="text-black" placeholder="Enter Username" />
-      <button className="text-black">Submit</button>
+      <input onChange={(e)=>setUser(e.target.value)} className="text-black" placeholder="Enter Username" />
+      <input onChange={(e)=>setPass(e.target.value)} className="text-black" placeholder="Enter Pass" />
+      <button onClick={handleLogin} className="text-black">Submit</button>
     </div>
   )
 }
